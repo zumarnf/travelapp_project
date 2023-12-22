f9f332b84030afdd45c7b0909ec89b09bc9bd9be
zumzum <kazonima@gmail.com>
2023-12-22 16:35:16 +0700

    startscreen3

--- lib/startscreen/startscreen.dart
+++ lib/startscreen/startscreen.dart
@@ -11,74 +11,85 @@ class _StartscreenState extends State<Startscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          // Gambar di bagian atas
-          Column(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(top: 5, bottom: 10, right: 7, left: 7),
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Image.asset(
-                'assets/img/gambar1.jpg', // Ganti dengan path gambar Anda
-                height: 270, // Atur tinggi gambar sesuai kebutuhan
-                width: MediaQuery.of(context)
-                    .size
-                    .width, // Lebar gambar sesuai lebar layar
-                fit: BoxFit
-                    .cover, // Ini akan membuat gambar menutupi ruang yang tersedia
+              // Gambar di bagian atas
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    'assets/img/gambar1.jpg',
+                    height: 270,
+                    width: MediaQuery.of(context).size.width,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(height: 10),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      'Discover the ideal destination for your next vacation.',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      '“Welcome to the journey  of a lifetime! Let’s get started on your adventure. Set your preferences, discover new places, and create memories that last a lifetime. Ready to explore?”',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w200,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(height: 15),
-              Text(
-                'Teks Anda di sini',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold, // Membuat teks menjadi tebal
-                  fontSize: 18, // Ukuran teks yang diinginkan
+              SizedBox(height: 30),
+
+              // Tombol Login
+              ElevatedButton(
+                onPressed: () {
+                  // Aksi untuk tombol login
+                  print('Tombol Login ditekan');
+                },
+                child: Text('Login', style: TextStyle(fontSize: 18)),
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                  backgroundColor: Color(0xFFFF9F5A),
+                  foregroundColor: Colors.white,
+                  minimumSize: Size(300, 40),
                 ),
               ),
-              Text(
-                'Teks Anda di sini',
-                style: TextStyle(
-                  fontWeight: FontWeight.w200, // Membuat teks menjadi tebal
-                  fontSize: 15, // Ukuran teks yang diinginkan
+              SizedBox(height: 20),
+
+              // Tombol Sign Up
+              ElevatedButton(
+                onPressed: () {
+                  // Aksi untuk tombol sign up
+                  print('Tombol Sign Up ditekan');
+                },
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(fontSize: 18),
+                ),
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                  backgroundColor: Color(0xFFFF9F5A),
+                  foregroundColor: Colors.white,
+                  minimumSize: Size(300, 50),
                 ),
               ),
             ],
           ),
-
-          SizedBox(height: 30),
-
-          // Tombol Login
-          ElevatedButton(
-            onPressed: () {
-              // Aksi untuk tombol login
-              print('Tombol Login ditekan');
-            },
-            child: Text('Login'),
-            style: ElevatedButton.styleFrom(
-              padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-              backgroundColor: Color(0xFFFF9F5A), // Warna latar belakang FF9F5A
-              foregroundColor: Colors.white, // Warna teks putih (FFFFFF)
-              minimumSize: Size(150, 50), // Ukuran minimum untuk tombol
-            ),
-          ),
-          SizedBox(height: 20), // Jarak antara tombol Login dan Sign Up
-
-          // Tombol Sign Up
-          ElevatedButton(
-            onPressed: () {
-              // Aksi untuk tombol sign up
-              print('Tombol Sign Up ditekan');
-            },
-            child: Text('Sign Up'),
-            style: ElevatedButton.styleFrom(
-              padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-              backgroundColor: Color(0xFFFF9F5A), // Warna latar belakang FF9F5A
-              foregroundColor: Colors.white, // Warna teks putih (FFFFFF)
-              minimumSize: Size(150, 50), // Ukuran minimum untuk tombol
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
