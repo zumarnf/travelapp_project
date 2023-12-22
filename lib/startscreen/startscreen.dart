e4b52e519019c95e0b9e278ed68f5a128553e8fe
zumzum <kazonima@gmail.com>
2023-12-22 23:07:49 +0700

    startscreenprogress

--- lib/startscreen/startscreen.dart
+++ lib/startscreen/startscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Startscreen extends StatefulWidget {
   const Startscreen({super.key});
@@ -22,18 +23,22 @@ class _StartscreenState extends State<Startscreen> {
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Image.asset(
-                    'assets/img/gambar1.jpg',
-                    height: 270,
-                    width: MediaQuery.of(context).size.width,
-                    fit: BoxFit.cover,
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(
+                        27), // Mengatur sudut menjadi bulat
+                    child: Image.asset(
+                      'assets/img/gambar1.jpg',
+                      height: 413,
+                      width: MediaQuery.of(context).size.width,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                   SizedBox(height: 10),
                   Padding(
                     padding: EdgeInsets.symmetric(horizontal: 10),
                     child: Text(
                       'Discover the ideal destination for your next vacation.',
-                      style: TextStyle(
+                      style: GoogleFonts.poppins(
                         fontWeight: FontWeight.bold,
                         fontSize: 22,
                       ),
@@ -44,7 +49,7 @@ class _StartscreenState extends State<Startscreen> {
                     padding: EdgeInsets.symmetric(horizontal: 10),
                     child: Text(
                       '“Welcome to the journey  of a lifetime! Let’s get started on your adventure. Set your preferences, discover new places, and create memories that last a lifetime. Ready to explore?”',
-                      style: TextStyle(
+                      style: GoogleFonts.poppins(
                         fontWeight: FontWeight.w200,
                         fontSize: 12,
                       ),
@@ -52,7 +57,7 @@ class _StartscreenState extends State<Startscreen> {
                   ),
                 ],
               ),
-              SizedBox(height: 30),
+              SizedBox(height: 40),
 
               // Tombol Login
               ElevatedButton(
@@ -60,17 +65,22 @@ class _StartscreenState extends State<Startscreen> {
                   // Aksi untuk tombol login
                   print('Tombol Login ditekan');
                 },
-                child: Text('Login', style: TextStyle(fontSize: 18)),
+                child: Text('Login', style: GoogleFonts.poppins(fontSize: 18)),
                 style: ElevatedButton.styleFrom(
                   padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
                   backgroundColor: Color(0xFFFF9F5A),
                   foregroundColor: Colors.white,
-                  minimumSize: Size(300, 40),
+                  minimumSize: Size(320, 40),
+                  shape: RoundedRectangleBorder(
+                    // Mengatur bentuk menjadi bulat
+                    borderRadius: BorderRadius.circular(
+                        27), // Anda dapat menyesuaikan nilai untuk membuatnya lebih bulat atau kurang bulat
+                  ),
                 ),
               ),
               SizedBox(height: 20),
 
-              // Tombol Sign Up
+// Tombol Sign Up
               ElevatedButton(
                 onPressed: () {
                   // Aksi untuk tombol sign up
@@ -78,15 +88,20 @@ class _StartscreenState extends State<Startscreen> {
                 },
                 child: Text(
                   'Sign Up',
-                  style: TextStyle(fontSize: 18),
+                  style: GoogleFonts.poppins(fontSize: 18),
                 ),
                 style: ElevatedButton.styleFrom(
                   padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
                   backgroundColor: Color(0xFFFF9F5A),
                   foregroundColor: Colors.white,
-                  minimumSize: Size(300, 50),
+                  minimumSize: Size(320, 50),
+                  shape: RoundedRectangleBorder(
+                    // Mengatur bentuk menjadi bulat
+                    borderRadius: BorderRadius.circular(
+                        27), // Anda dapat menyesuaikan nilai untuk membuatnya lebih bulat atau kurang bulat
+                  ),
                 ),
-              ),
+              )
             ],
           ),
         ),
