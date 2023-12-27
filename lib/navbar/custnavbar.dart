0c84b978a77446aad5bd1d743380e6afa19f551c
Josey <josetakesan46@gmail.com>
2023-12-27 15:16:16 +0700

    commit lagi

--- lib/navbar/custnavbar.dart
+++ lib/navbar/custnavbar.dart
@@ -4,47 +4,47 @@ import 'package:flutter/material.dart';
 // main app that calls the runApp.
 
 class AnimatedBottomBar extends StatefulWidget {
-AnimatedBottomBar({Key? key}) : super(key: key);
+  AnimatedBottomBar({Key? key}) : super(key: key);
 
-@override
-State<AnimatedBottomBar> createState() => _AnimatedBottomBarState();
+  @override
+  State<AnimatedBottomBar> createState() => _AnimatedBottomBarState();
 }
 
 class _AnimatedBottomBarState extends State<AnimatedBottomBar> {
 // list of icons that required
 // by animated navigation bar
-List<IconData> iconList = [ 
-	Icons.abc_sharp,
-	Icons.access_time,
-	Icons.holiday_village,
-	Icons.account_tree_rounded
-];
+  List<IconData> iconList = [
+    Icons.abc_sharp,
+    Icons.access_time,
+    Icons.holiday_village,
+    Icons.account_tree_rounded
+  ];
 
-// default index of the tabs 
-int _bottomNavIndex = 0; 
-@override
-Widget build(BuildContext context) {
-	// material app with
-	// debugshowcheckedmodebanner false
-	return MaterialApp( 
-	debugShowCheckedModeBanner: false,
-	home: Scaffold(
-		body: Container(), 
-		floatingActionButton: FloatingActionButton(
-		
+// default index of the tabs
+  int _bottomNavIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    // material app with
+    // debugshowcheckedmodebanner false
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(),
+        floatingActionButton: FloatingActionButton(
+        
 			child: Icon(Icons.home_max_outlined),
 			onPressed: () {}),
 		floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
 		bottomNavigationBar: AnimatedBottomNavigationBar( // navigation bar
-		icons: iconList,
-		activeIndex: _bottomNavIndex,
-		gapLocation: GapLocation.center,
-		notchMargin: 8, // Default notch margin is 8
-		notchSmoothness: NotchSmoothness.verySmoothEdge,
-		onTap: (index) => setState(() => _bottomNavIndex = index),
-		backgroundColor: Colors.blue,
-		),
-	),
-	);
-}
+          icons: iconList,
+          activeIndex: _bottomNavIndex,
+          gapLocation: GapLocation.center,
+          notchMargin: 8, // Default notch margin is 8
+          notchSmoothness: NotchSmoothness.verySmoothEdge,
+          onTap: (index) => setState(() => _bottomNavIndex = index),
+          backgroundColor: Colors.blue,
+        ),
+      ),
+    );
+  }
 }
