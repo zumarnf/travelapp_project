adb3bad8d5c5a3873c123ef849b4f91d2d79cd73
zumzum <kazonima@gmail.com>
2023-12-27 12:48:35 +0700

    locate2

--- lib/locationscreen/locationpage.dart
+++ lib/locationscreen/locationpage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 
 class locationpg extends StatefulWidget {
   @override
@@ -54,21 +54,53 @@ class _locationpgState extends State<locationpg> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Geolocator Example'),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Icon(
+              Icons
+                  .location_on_outlined, // Anda dapat mengganti ikon dengan yang lain jika diinginkan
+              size: 75, // Ukuran ikon
+              color: const Color.fromARGB(255, 0, 0, 0), // Warna ikon
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20, top: 25),
+              child: Text("PLEASE ALLOW LOCATION TO GET LOCATION",
+                  style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w700, fontSize: 12)),
+            ),
             if (isLoading)
               CircularProgressIndicator()
             else
               ElevatedButton(
                 onPressed: getLocation,
                 child: Text('Get Location'),
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                  backgroundColor: Color(0xFFFF9F5A),
+                  foregroundColor: Colors.white,
+                  minimumSize: Size(261, 44),
+                  shape: RoundedRectangleBorder(
+                    // Mengatur bentuk menjadi bulat
+                    borderRadius: BorderRadius.circular(
+                        27), // Anda dapat menyesuaikan nilai untuk membuatnya lebih bulat atau kurang bulat
+                  ),
+                ),
               ),
-            Text("Latitude : $latitude, Longtitude : $longitude"),
+            SizedBox(height: 15),
+            Column(
+              children: [
+                Text(
+                  "YOUR LOCATION",
+                  style: GoogleFonts.poppins(
+                      fontSize: 18, fontWeight: FontWeight.w700),
+                ),
+                Text("$latitude, $longitude",
+                    style: GoogleFonts.poppins(
+                        fontSize: 20, fontWeight: FontWeight.w500)),
+              ],
+            ),
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travelapp/locationscreen/locationpage.dart';
 
 import 'package:travelapp/startscreen/startscreen.dart';
 
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Startscreen(),
+      home: locationpg(),
     );
   }
 }
